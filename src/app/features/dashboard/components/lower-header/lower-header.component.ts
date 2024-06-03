40cd28108b5d2b026fa73473992015e91a325d2c
shrouk <shrouk@yahoo.com>
2024-06-03 11:40:10 +0300

    done

--- src/app/features/dashboard/components/lower-header/lower-header.component.ts
+++ src/app/features/dashboard/components/lower-header/lower-header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MainService } from '../../services/main.service';
+import { IProgramm } from '../../interface/programm';
 
 @Component({
   selector: 'app-lower-header',
   templateUrl: './lower-header.component.html',
   styleUrls: ['./lower-header.component.scss']
 })
-export class LowerHeaderComponent {
+export class LowerHeaderComponent implements OnInit {
+  ProgramList: IProgramm[] = [];
 
+  constructor(private _MainService: MainService) { }
+  ngOnInit(): void {
+    this._MainService.getAll().subscribe({
+      next: (res) => {
+        this.ProgramList = res;
+        console.log(this.ProgramList);
+      }, error(err) {
+
+      }, complete() {
+
+      },
+    })
+  }
 }
